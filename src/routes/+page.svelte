214980d642d2249ca0c39827c8eb73655c77314f
sreemaye <sreemaye44@gmail.com>
2023-09-03 11:54:50 +0600

    complete the mountain of layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,8 +21,9 @@
     <!-- mountain home -->
     <div class="bg-yellow-100 col-span-2 grid place-items-center">
            <div class="h-[300px] w-[300px] bg-[#293462] grid place-items-center">
-            <div class="h-[200px] w-[200px] bg-yellow-100 rounded-full">
-
+            <div class="inner-circle">
+<div class="big-box"></div>
+<div class="smaller-box"></div>
             </div>
            </div>
     </div>
@@ -30,5 +31,39 @@
   </div>
 </div>
   </div>
+
+  <style>
+
+.inner-circle{
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: #FFF1C1;
+  overflow: hidden;
+  position: relative;
+}
+.big-box{
+      width: 0px;
+      height: 0px;
+      border-bottom: 200px solid #FE5F55;
+      border-right: 200px solid transparent;
+      border-left: 200px solid transparent;
+      position: absolute;
+      top: 59px;
+      left: -59px;
+
+    }
+.smaller-box{
+      width: 0px;
+      height: 0px;
+      border-bottom: 100px solid #FE5F55;
+      border-right: 100px solid transparent;
+      border-left: 100px solid transparent;
+      position: absolute;
+      top: 129px;
+      left: -71px;
+
+    }
+  </style>
 
 
