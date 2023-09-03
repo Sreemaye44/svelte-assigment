3d2e01a18e8dc184dc5bc688cdbebb2641e4d580
sreemaye <sreemaye44@gmail.com>
2023-09-03 11:39:10 +0600

    right side portion of layout done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
   <!-- left part -->
 <div class="grid grid-rows-6">
   <!-- upper part -->
-  <div class="bg-red-100 row-span-5 grid grid-cols-3">
-  <div class="bg-slate-200 grid grid-row-1">
+  <div class="row-span-5 grid grid-cols-3">
+  <div class="grid grid-row-1">
     <div class="bg-blue-400 row-span-3"></div>
     <div class="bg-teal-400 row-span-2"></div>
    
@@ -15,7 +15,20 @@
 <div class="bg-fuchsia-400"></div>
 </div>
 <!-- right part -->
-<div class="bg-red-200"></div>
+<div class=" grid grid-row-3">
+  <div class="bg-blue-400"></div>
+  <div class="row-span-2 grid grid-cols-3">
+    <!-- mountain home -->
+    <div class="bg-yellow-100 col-span-2 grid place-items-center">
+           <div class="h-[300px] w-[300px] bg-[#293462] grid place-items-center">
+            <div class="h-[200px] w-[200px] bg-yellow-100 rounded-full">
+
+            </div>
+           </div>
+    </div>
+    <div class="bg-emerald-400"></div>
+  </div>
+</div>
   </div>
 
 
