9ae7d683e29587f0baa6f8110e43b84eaf56eb56
sreemaye <sreemaye44@gmail.com>
2023-09-03 11:03:59 +0600

    left part layout complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  
-  <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
-    }
-  </style>
+
+  <div class="grid grid-cols-2 h-screen">
+  <!-- left part -->
+<div class="grid grid-rows-6">
+  <!-- upper part -->
+  <div class="bg-red-100 row-span-5 grid grid-cols-3">
+  <div class="bg-slate-200 grid grid-row-1">
+    <div class="bg-blue-400 row-span-3"></div>
+    <div class="bg-teal-400 row-span-2"></div>
+   
+  </div>
+  <div class=" bg-rose-100 col-span-2"></div>
+</div>
+<!-- lower part -->
+<div class="bg-fuchsia-400"></div>
+</div>
+<!-- right part -->
+<div class="bg-red-200"></div>
+  </div>
+
+
